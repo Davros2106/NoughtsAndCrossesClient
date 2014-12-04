4ae2d397ec0cd3787c609a8874f0052f8fc9dc1b
Davros2106 <david.robson@tombola-4.local>
2014-12-04 14:09:54 +0000

    Basic structure of MVC and server response functionality created.

--- main-app/app/scripts/app.js
+++ main-app/app/scripts/app.js
@@ -1,16 +1,40 @@
 var noughtsAndCrossesApp = angular.module('noughtsAndCrossesApp', []);
 
-noughtsAndCrossesApp.factory('gameModel', function() {
+noughtsAndCrossesApp.factory('gameModel', function($http) {
 
 return {
     outcome: 'continue',
     gameState: '000000000',
-    winner: '0'
+    winner: 0,
+    player1:'random',
+    player2:'random',
 
-       };
 
-}
-    );
+
+    serverPost : {
+        method: 'POST',
+        url: '',
+        'withCredentials': 'true',
+        headers: {
+            'content-type': 'application/json'
+        },
+        data: ''
+
+    },
+
+    StartNewGame: function () {
+        var me = this;
+
+        me.serverPost.url = 'http://tictactoe1.cloudapp.net:35000/api/v1.0/newgame';
+        me.serverPost.data = {'player1': me.player1, 'player2': me.player2};
+        $http(me.serverPost)
+
+
+            .success(function (data) {
+                me.outcome = data.outcome;
+            });
+        }};
+});
 
 
 
