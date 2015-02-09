6ebb9a2732e519044a23090a93dca0000b374634
Davros2106 <david.robson@tombola-3.local>
2015-02-09 11:02:06 +0000

    Module structure and clean-up.

--- main-app/app/scripts/providers/audio/audioService.js
+++ main-app/app/scripts/providers/audio/audioService.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    angular.module('tombola.noughtsAndCrosses')
+    angular.module('tombola.noughtsAndCrosses.services.audio')
 
         .service('audioService', function($timeout, $document) {
 
