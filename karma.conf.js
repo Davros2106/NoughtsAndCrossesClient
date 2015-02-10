6546e5995ffdffebd8ac774f2cbbd88a3968cae2
Davros2106 <david.robson@tombola-5.local>
2015-02-10 11:34:21 +0000

    Added All relevant files for Karma. KarmaTask, karma.conf.js and changed grunt file and package.json.

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+
+
+    config.set({
+
+        basePath: '',
+
+        autoWatch: false,
+
+        browsers: ['PhantomJS'],
+
+        frameworks: ['mocha','chai'],
+
+        logLevel: config.LOG_INFO,
+
+        port: 9876,
+
+        reporters: ['nyan'],
+
+        colors: true,
+
+        singleRun: true,
+
+    });
+
+};
